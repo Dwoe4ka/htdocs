054f253895fb0756061579454ecdb48124420e00
Dwoe4ka~ <sandrij.prof@gmail.com>
2023-11-30 14:46:21 +0200

    ADDED DATABASE USAGE

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php //session_start() ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +7,52 @@
     <title>PHP</title>
 </head>
 <body>
-    <form action = "/injection.php" method="post">
-        <label for="name_surname">Вкажіть ім'я та прізвище</label>
-        <input type="text", name="name_surname"><br>
-        <label for="password">Вкажіть пароль</label>
-        <input type="text", name="password"><br>
-        <button type="submit">Надіслати до Аллаха</button>
+    
     <?php 
-    require 'header.php';
-    require 'footer.php';
+    /*
+    $user = 'root';
+    $password = 'root';
+    $db = 'users';
+    $host = 'localhost';
+    $port = 3306;
+    $dsn = 'mysql:host'.$host.';dbname='.$db;
+    $pdo = new PDO($dsn, $user, $password);
+    $query = $pdo->query('SELECT * FROM `users`');
+    //require 'header.php';
+    if ($_SESSION['name_surname'] != "")
+    echo 'Ім\'я та прізвище: ' . $_SESSION['name_surname'] . '<br>';
+    if ($_SESSION['password'] != "")
+    echo 'Пароль: ' . $_SESSION['password'] . '<br>';
+    //require 'footer.php'; 
+    //$name_surname = $_POST['name_surname'];
+    //$password = $_POST['password'];
+    ////if ($_COOKIE['name_surname'] != "" || $_COOKIE['account_password'] != "") {
+    ////echo 'Ім\'я та прізвище: ' . $_COOKIE['name_surname'] . '<br>';
+    ////echo 'Пароль: ' . $_COOKIE['account_password'] . '<br>';
+    ////}
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        echo $row['id']. ' ';
+        echo $row['name_surname'].' ';
+        echo $row['c_amount'];
+    }
+    */
+    try {
+        $dsn = "mysql:host=localhost;dbname=users";
+        $user = "root";
+        $passwd = "root";
+        $pdo = new PDO($dsn, $user, $passwd);
+        echo 'Done!<br>';
+        $query = $pdo->query('SELECT * FROM users');
+        while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+            echo 'TEXT ID->'.$row->id.'<br>';
+            echo 'TEXT NS->'.$row->name_surname.'<br>';
+            echo 'TEXT AM->'.$row->c_amount.'<br>';
+        }
+    }
+    catch (PDOException $e) {
+         echo "Error!: " . $e->getMessage() . "<br/>";
+         die();
+     }
     ?>
-</body>
-</html>
+</body> 
+</html> 
--- injection.php
+++ injection.php
@@ -1,4 +1,8 @@
 <?php
-echo $_POST['name_surname'].'<br>';
-echo $_POST['password'].'<br>';
+//session_start();
+header ('Location: /');
+//$_SESSION['name_surname'] = $_POST['name_surname'];
+//$_SESSION['password'] = $_POST['password'];
+setcookie("name_surname", $_POST['name_surname'], time () + 60);
+setcookie("account_password", $_POST['account_password'], time () + 60);
 ?>
